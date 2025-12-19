cd1dc5408799c9e1e8f13bdd9fcc11f6880ef60c
Tanmay Tyagi <tanmaytyagi222@gmail.com>
2025-12-19 19:38:41 +0530

    added more patterns question

---
+++ src/main/java/com/dsa/patterns/TwoPointers/ReverseWords.java
@@ -0,0 +1,49 @@
+package com.dsa.patterns.TwoPointers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ReverseWords {
+
+    public static String reverseWords(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        List<String> tokens = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+            tokens.add(st.nextToken());
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = tokens.size() - 1; i >= 0; i--) {
+            sb.append(tokens.get(i));
+            if (i != 0) sb.append(' ');
+        }
+        return sb.toString();
+    }
+
+    public static String reverseWordsInPlace(String s) {
+        int i = s.length() - 1;
+        StringBuilder sb = new StringBuilder();
+
+        while (i >= 0) {
+            // Skip trailing spaces
+            while (i >= 0 && s.charAt(i) == ' ') i--;
+
+            if (i < 0) break;
+
+            // Find the start of the word
+            int end = i;
+            while (i >= 0 && s.charAt(i) != ' ') i--;
+
+            // Append the word
+            if (!sb.isEmpty()) sb.append(' ');
+            sb.append(s, i + 1, end + 1);
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "       Let's take  the  HackerCode contest        ";
+        System.out.println(reverseWordsInPlace(str));
+    }
+}
---
+++ src/main/java/com/dsa/patterns/TwoPointers/ValidWordAbbreviation.java
@@ -0,0 +1,32 @@
+package com.dsa.patterns.TwoPointers;
+
+/**
+ * "cal3ar" and "calendar" are valid word abbreviation, whereas
+ * "c24r" and "calendar" are not.
+ */
+
+public class ValidWordAbbreviation {
+    public static boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0;
+        int j = 0;
+
+        while (i < word.length() && j < abbr.length()) {
+            if(abbr.charAt(j) == '0') return false;
+            if(Character.isDigit(abbr.charAt(j))) {
+                int k = j;
+                while(j < abbr.length() && Character.isDigit(abbr.charAt(j))) j++;
+                int num = Integer.parseInt(abbr.substring(k,j));
+                if(i + num > word.length()) return false;
+                i += num;
+            } else {
+                if(word.charAt(i++) != abbr.charAt(j++)) return false;
+            }
+        }
+
+        return i == word.length() && j == abbr.length();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(validWordAbbreviation("abcdefg", "1b3f1"));
+    }
+}

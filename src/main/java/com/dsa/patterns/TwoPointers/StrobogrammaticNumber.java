42e3b2184e022ff778f3260bba2938492620f181
Tanmay Tyagi <tanmaytyagi222@gmail.com>
2025-12-19 21:36:22 +0530

    added strobogrammatic number question and test

---
+++ src/main/java/com/dsa/patterns/TwoPointers/StrobogrammaticNumber.java
@@ -0,0 +1,37 @@
+package com.dsa.patterns.TwoPointers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StrobogrammaticNumber {
+
+    private static final Map<Character, Character> map = new HashMap<>();
+    static {
+        map.put('6', '9');
+        map.put('9', '6');
+        map.put('1', '1');
+        map.put('8', '8');
+        map.put('0', '0');
+    }
+
+    public static boolean isStrobogrammatic(String num) {
+        if(num == null) throw new NullPointerException("num is null");
+        if(num.isEmpty()) throw new IllegalArgumentException("num is empty");
+
+        int i = 0;
+        int j = num.length() - 1;
+
+        while(i <= j) {
+            char ci = num.charAt(i);
+            char cj = num.charAt(j);
+            if(!map.containsKey(ci) ||  map.get(ci) != cj) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isStrobogrammatic("96"));
+    }
+}
---
+++ src/test/java/com/dsa/patterns/TwoPointers/StrobogrammaticNumberTest.java
@@ -0,0 +1,33 @@
+package com.dsa.patterns.TwoPointers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StrobogrammaticNumberTest {
+    @Test
+    void testValidStrobogrammaticNumber() {
+        assertTrue(StrobogrammaticNumber.isStrobogrammatic("69"));
+        assertTrue(StrobogrammaticNumber.isStrobogrammatic("808"));
+        assertTrue(StrobogrammaticNumber.isStrobogrammatic("101"));
+    }
+
+    @Test
+    void testInvalidStrobogrammaticNumber() {
+        assertFalse(StrobogrammaticNumber.isStrobogrammatic("a"));
+        assertFalse(StrobogrammaticNumber.isStrobogrammatic("2"));
+        assertFalse(StrobogrammaticNumber.isStrobogrammatic("89"));
+    }
+
+    @Test
+    void testEmptyString() {
+        assertThrows(IllegalArgumentException.class, () ->
+                StrobogrammaticNumber.isStrobogrammatic(""));
+    }
+
+    @Test
+    void testNullInput() {
+        assertThrows(NullPointerException.class, () ->
+                StrobogrammaticNumber.isStrobogrammatic(null));
+    }
+}

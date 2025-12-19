53fdab4994ae65410865442576e4bbc18ab44b91
Tanmay Tyagi <tanmaytyagi222@gmail.com>
2025-12-20 02:12:30 +0530

    added min swap to palindrom problem

---
+++ src/main/java/com/dsa/patterns/TwoPointers/MinAdjacentSwapToMakePalindrome.java
@@ -0,0 +1,45 @@
+package com.dsa.patterns.TwoPointers;
+
+public class MinAdjacentSwapToMakePalindrome {
+
+    public static int minMovesToMakePalindrome(String s) {
+        int ans = 0;
+
+        while(s.length() > 2) {
+            int len = s.length();
+
+            char ch1 = s.charAt(0);
+            char ch2 = s.charAt(len - 1);
+
+            if(ch1 == ch2) {
+                s = s.substring(1, len - 1);
+            } else {
+                int id1 = s.lastIndexOf(ch1);
+                int id2 = s.indexOf(ch2);
+
+                int step1 = len - id1 - 1;
+                int step2 = id2;
+
+                StringBuilder sb = new StringBuilder();
+
+                if(step1 > step2) {
+                    ans += step2;
+                    sb.append(s, 0, id2);
+                    sb.append(s, id2 + 1, len - 1);
+                } else {
+                    ans += step1;
+                    sb.append(s, 1, id1);
+                    sb.append(s, id1 + 1, len);
+                }
+
+                s = sb.toString();
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minMovesToMakePalindrome("mamad"));
+    }
+}
---
+++ src/test/java/com/dsa/patterns/TwoPointers/MinAdjacentSwapToMakePalindromeTest.java
@@ -0,0 +1,14 @@
+package com.dsa.patterns.TwoPointers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinAdjacentSwapToMakePalindromeTest {
+
+    @Test
+    void validTestCases() {
+        assertEquals(0, MinAdjacentSwapToMakePalindrome.minMovesToMakePalindrome("abba"));
+        assertEquals(1, MinAdjacentSwapToMakePalindrome.minMovesToMakePalindrome("abab"));
+    }
+}

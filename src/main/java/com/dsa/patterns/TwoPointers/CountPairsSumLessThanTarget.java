1b278f4c4e773aaf5b674e1e65510966444e91dc
Tanmay Tyagi <tanmaytyagi222@gmail.com>
2025-12-20 17:00:22 +0530

    added count pairs sum less than target problem

---
+++ src/main/java/com/dsa/patterns/TwoPointers/CountPairsSumLessThanTarget.java
@@ -0,0 +1,34 @@
+package com.dsa.patterns.TwoPointers;
+import java.util.Arrays;
+
+
+/**
+ * count number of pairs (i,j) 0 ≤ i < j < n such that a[i]+a[j] is strictly less than the k.
+ * @Time - O(nlogn)
+ * @Space - O(n)
+ */
+public class CountPairsSumLessThanTarget {
+    public static int countPairsSumLessThanTarget(int[] a, int k) {
+        Arrays.sort(a);
+
+        int count = 0;
+        int i = 0;
+        int j = a.length - 1;
+
+        while (i < j) {
+            if(a[i] + a[j] >= k) j--;
+            else {
+                count += j - i;
+                i++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,2,6,8,-1,0,-4,12,1};
+        int target = 6;
+        System.out.println(countPairsSumLessThanTarget(arr, target));
+    }
+}
---
+++ src/test/java/com/dsa/patterns/TwoPointers/CountPairsSumLessThanTargetTest.java
@@ -0,0 +1,22 @@
+package com.dsa.patterns.TwoPointers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountPairsSumLessThanTargetTest {
+
+    @Test
+    void validTestCase() {
+        int[] arr = {3,2,6,8,-1,0,-4,12,1};
+        int target = 6;
+        assertEquals(18, CountPairsSumLessThanTarget.countPairsSumLessThanTarget(arr, target));
+    }
+
+    @Test
+    void invalidTestCase() {
+        int[] arr = {3,3};
+        int target = 6;
+        assertEquals(0, CountPairsSumLessThanTarget.countPairsSumLessThanTarget(arr, target));
+    }
+}
